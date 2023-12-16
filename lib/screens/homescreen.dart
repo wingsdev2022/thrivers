3e478215148170133764a6b66c4f5c204a8f7c21
FLYINGSKATE <ASHRAFKHAN84@OUTLOOK.COM>
2023-12-16 20:35:01 +0530

    UI layed out FIX

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,12 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tree/flutter_tree.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:multi_select_flutter/dialog/multi_select_dialog_field.dart';
 import 'package:multi_select_flutter/util/multi_select_item.dart';
 import 'package:multi_select_flutter/util/multi_select_list_type.dart';
-import 'package:flutter_tree_pro/flutter_tree_pro.dart';
 import '../Network/FirebaseApi.dart';
 import '../core/apphelper.dart';
 import '../core/constants.dart';
@@ -49,7 +49,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   List<String> mySelectedUsers = [];
 
-  bool showTreeView = false;
+  bool showTreeView = true;
 
   static List<Animal> _animals = [
     Animal(id: 1, name: "Lion"),
@@ -107,6 +107,43 @@ class _HomeScreenState extends State<HomeScreen> {
     {"parentId": 1009, "value": "镇江市", "id": 1134},
   ];
 
+  Future<List<TreeNodeData>> _load(TreeNodeData parent) async {
+    await Future.delayed(const Duration(seconds: 1));
+    final data = [
+      TreeNodeData(
+        title: 'Load node 1',
+        expaned: false,
+        checked: true,
+        children: [],
+        extra: null,
+      ),
+      TreeNodeData(
+        title: 'Load node 2',
+        expaned: false,
+        checked: false,
+        children: [],
+        extra: null,
+      ),
+    ];
+
+    return data;
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+   /// var response = await rootBundle.loadString('assets/data.json');
+    setState(() {
+      initialTreeData.forEach((item) {
+        treeListData.add(item);
+      });
+    });
+  }
 
 
   @override
@@ -382,7 +419,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   }).toList();
                 }
 
-                return showTreeView? ListView.separated(
+                return !showTreeView? ListView.separated(
                   reverse: true,
                   shrinkWrap: true,
                   physics: BouncingScrollPhysics(),
@@ -425,15 +462,51 @@ class _HomeScreenState extends State<HomeScreen> {
                   );*/
                   },
                 ):Container(
-                  child: FlutterTreePro(
-                    listData: treeListData,
-                    initialListData: initialTreeData,
-                    config: Config(
-                      parentId: 'parentId',
-                      dataType: DataType.DataList,
-                      label: 'value',
-                    ),
-                    onChecked: (List<Map<String, dynamic>> checkedList) {},
+                  child: TreeView(
+                    data: treeData,
+                    lazy: true,
+                    load: _load,
+                    showActions: true,
+                    showCheckBox: true,
+                    showFilter: true,
+                    append: (parent) {
+                      print(parent.extra);
+                      return TreeNodeData(
+                        title: 'Appended',
+                        expaned: true,
+                        checked: true,
+                        children: [],
+                      );
+                    },
+                    onLoad: (node) {
+                      print('onLoad');
+                      print(node);
+                    },
+                    onAppend: (node, parent) {
+                      print('onAppend');
+                      print(node);
+                    },
+                    onCheck: (checked, node) {
+                      print('checked');
+                      print('onCheck');
+                      print(node);
+                    },
+                    onCollapse: (node) {
+                      print('onCollapse');
+                      print(node);
+                    },
+                    onExpand: (node) {
+                      print('onExpand');
+                      print(node);
+                    },
+                    onRemove: (node, parent) {
+                      print('onRemove');
+                      print(node);
+                    },
+                    onTap: (node) {
+                      print('onTap');
+                      print(node);
+                    },
                   ),
                 );
               },
